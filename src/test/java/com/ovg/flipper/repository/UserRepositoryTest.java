a0e4a5ae895e128f887daabfe8d8da646f2f3ac0
Choi Jun Ho <jjun3511@naver.com>
2024-09-27 23:54:35 +0900

    refactor : Warning 제거.

--- src/test/java/com/ovg/flipper/repository/UserRepositoryTest.java
+++ src/test/java/com/ovg/flipper/repository/UserRepositoryTest.java
@@ -5,12 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
-
-import java.util.HashSet;
-import java.util.Set;
-
 import static org.junit.jupiter.api.Assertions.*;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @ActiveProfiles("test")
