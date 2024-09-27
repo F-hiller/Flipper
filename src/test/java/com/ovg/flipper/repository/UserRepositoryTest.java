752ce74497e421a7165e8e5ec1544871667f6f37
Choi Jun Ho <jjun3511@naver.com>
2024-09-27 23:50:26 +0900

    test(UserRepository) : 레포지토리 테스트.

---
+++ src/test/java/com/ovg/flipper/repository/UserRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.ovg.flipper.repository;
+
+import com.ovg.flipper.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testSaveUser() {
+        User user = User.builder()
+                .username("testUser")
+                .password("testPassword")
+                .email("test@gmail.com")
+                .build();
+
+        User savedUser = userRepository.save(user);
+        assertNotNull(savedUser.getUserId());
+        assertEquals(user.getUsername(), savedUser.getUsername());
+        assertEquals(user.getEmail(), savedUser.getEmail());
+        assertEquals(user.getPassword(), savedUser.getPassword());
+        assertEquals(user.getRole(), "ROLE_USER");
+    }
+}
