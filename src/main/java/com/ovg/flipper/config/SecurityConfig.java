161091705efb9dca072193b250f1cee04edd23d1
Choi Jun Ho <jjun3511@naver.com>
2024-10-02 19:14:05 +0900

    fix(login) : 로그인 후 원래 페이지로 이동.

--- src/main/java/com/ovg/flipper/config/SecurityConfig.java
+++ src/main/java/com/ovg/flipper/config/SecurityConfig.java
@@ -19,7 +19,6 @@ public class SecurityConfig {
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 ).formLogin(form -> form.loginPage("/login")
-                        .defaultSuccessUrl("/chat", true)
                         .permitAll()
                         .failureUrl("/login?error=true"))
                 .logout(LogoutConfigurer::permitAll)
